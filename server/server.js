d378fa15bc171462af45aca872171b93714585d8
kbabajan <kbabajan@echolalia.(none)>
2013-06-16 07:27:52 +0200

    Sockety - poczatek

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
+(function() {
+//websockety:
+var io = require('socket.io').listen(9000);
+//Klasa weza
+var snake = require('./snake.js');
+//Klasa obiektow/jablek
+var obj  = require('./item.js');
+
+//http
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
-var snaker = express();
 //
+
+//Konfiguracja i uruchomienie serwera HTTP
+var snaker = express();
 snaker.configure(function() {
 
     snaker.set('port', process.env.PORT || 5000);
@@ -19,3 +29,36 @@ snaker.configure(function() {
 var server = http.createServer(snaker).listen(snaker.get('port'), function() {
     console.log("HTTP stoi na porcie " + snaker.get('port'));
 });
+//
+
+//tabela klientow podlaczonych do gry
+var clients = [];
+
+
+//Obsluga websocketow/zdarzen
+
+  var onConnect = function(socket){
+    io.sockets.emit('connect', {
+    });
+  }
+  var onDisconnect = function(socket){
+    socket.on('disconnect', function(){
+      console.log("Rozlaczono ");
+    });
+  }
+  
+  
+  //Wlasciwy server socket.io
+    io.sockets.on('connection', function(socket) {
+        //doczytac o socket.id
+        var id = socket.id;
+
+        //Obsluga wlasciwa
+        onConnect(socket);
+        onDisconnect(socket);
+  });
+
+
+//
+
+}).call(this);
